b294391c3d1461c67d23ea83dcc060a1927f42a8
AkyunaAkish <akyunaakish@gmail.com>
2016-09-08 14:31:56 -0600

    updated knex schema

--- migrations/20160906131644_blogs_schema.js
+++ migrations/20160906131644_blogs_schema.js
@@ -2,6 +2,8 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('blogs', function(table) {
     table.increments()
     table.string('title').notNullable()
+    table.string('thumbnail').notNullable()
+    table.text('description', 'longText').notNullable()
     table.text('content', 'longText').notNullable()
     table.integer('author_id').unsigned().references('users.id').onDelete('CASCADE').onUpdate('CASCADE').index()
     table.bigInteger('created_at').notNullable().defaultTo(Date.now())
--- migrations/20160908105603_rating_schema.js
+++ migrations/20160908105603_rating_schema.js
@@ -1,12 +1,13 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('likes', function(table) {
+  return knex.schema.createTable('ratings', function(table) {
     table.increments()
-    table.integer('liker_id').unsigned().references('users.id').onDelete('CASCADE').onUpdate('CASCADE').index()
+    table.float('rating', 2, 1).unsigned()
+    table.integer('user_id').unsigned().references('users.id').onDelete('CASCADE').onUpdate('CASCADE').index()
     table.integer('blog_id').unsigned().references('blogs.id').onDelete('CASCADE').onUpdate('CASCADE').index()
     table.bigInteger('created_at').notNullable().defaultTo(Date.now())
   })
 }
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('likes')
+  return knex.schema.dropTable('ratings')
 }
